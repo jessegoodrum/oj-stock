8025daeb467dbaa163055bd2fa8658faa500be39
Jesse <jessegoodrum@me.com>
2023-03-04 14:07:31 -0800

    changed display

--- src/components/ojCard/ojCard.component.jsx
+++ src/components/ojCard/ojCard.component.jsx
@@ -30,50 +30,47 @@ export default function OjCard({id,isInStock,name,price,quantity, updateQuantity
     return(
         
         <Card key={id} className="mx-auto oj-card-container">
-        <Card.Body>
-        <div className="d-flex align-items-center">
-            <div>
-              <Card.Img variant="top" src={images[id]} className="oj-img img-fluid" />
-            </div>
-            <div className="oj-card-data">
-              <Card.Title className="oj-title">{name}</Card.Title>
-              <div className="oj-info">
-                <Card.Text>Price: {price}</Card.Text>
-    
-                <div>
+        <Card.Body className="d-flex flex-column flex-sm-row align-items-center justify-content-center">
+          <div className="d-flex justify-content-center mb-3 mb-sm-0">
+            <Card.Img variant="top" src={images[id]} className="oj-img img-fluid" />
+          </div>
+          <div className="oj-card-data text-center text-sm-start">
+            <Card.Title className="oj-title">{name}</Card.Title>
+            <div className="oj-info">
+              <Card.Text>Price: {price}</Card.Text>
+              <div>
                 <Card.Text>Availability: {isInStock ? 'In Stock' : 'Not In Stock'}</Card.Text>
                 <Card.Text>Stock Amount: {quantity}</Card.Text>
-                </div>
-                
               </div>
-              <br />
-              <Col className="oj-update">
-                <InputGroup className="mb-3">
-                  <Form.Select
-                    placeholder="Enter Oj Quantity"
-                    aria-label="Enter Oj Quantity"
-                    aria-describedby="basic-addon2"
-                    type="number" 
-                    min="0" max="50"
-                    onChange={changeQuantity}
-                    list="defaultNumbers"
-                    required
-                  >
-                  <option value = '0'>Update OJ Stock</option>
-                    {Array.from({ length: 51 }, (_, i) => (
-                      <option key={i} value={i}>{i}</option>
-                    ))}
-                  </Form.Select>
-                  <Button variant="outline-secondary" id="button-addon2" onClick={() =>{handleClick(id, amount)}}>
-                    Update
-                  </Button>
-                </InputGroup>
-              </Col>
-              <Card.Text>Last Updated: {lastUpdated}</Card.Text>
             </div>
+            <br />
+            <Col className="oj-update">
+              <InputGroup className="mb-3">
+                <Form.Select
+                  placeholder="Enter Oj Quantity"
+                  aria-label="Enter Oj Quantity"
+                  aria-describedby="basic-addon2"
+                  type="number"
+                  min="0" max="50"
+                  onChange={changeQuantity}
+                  list="defaultNumbers"
+                  required
+                >
+                  <option value='0'>Update OJ Stock</option>
+                  {Array.from({ length: 51 }, (_, i) => (
+                    <option key={i} value={i}>{i}</option>
+                  ))}
+                </Form.Select>
+                <Button variant="outline-secondary" id="button-addon2" onClick={() => { handleClick(id, amount) }}>
+                  Update
+                </Button>
+              </InputGroup>
+            </Col>
+            <Card.Text>Last Updated: {lastUpdated}</Card.Text>
           </div>
         </Card.Body>
       </Card>
+
       
       
 
